324cee117e330890c8687ae80be5940bfff89286
Rafael Pina <rafaa.lb@hotmail.com>
2018-12-12 10:11:03 -0400

    Add main structure

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,14 @@
 const Mutation = {
+    async createItem(parent, args, ctx, info) {
+      // check if they are logged in
+      const item = await ctx.db.mutation.createItem({
+        data: {
+          ...args
+        }
+      }, info);
 
+      return item;
+    }
 };
 
 module.exports = Mutation;
